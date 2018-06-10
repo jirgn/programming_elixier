d5c3055da0f17174b6ae08a48fbeb673eecd6a2d
jirgn <jirgn76@googlemail.com>
2018-06-10 23:56:36 +0200

    some more list_and_recursion

--- lists_and_recursion/my_list.exs
+++ lists_and_recursion/my_list.exs
@@ -15,7 +15,7 @@ defmodule MyList do
     _max(tail, head)
   end
 
-  def caesar([], n), do: []
+  def caesar([], _n), do: []
   def caesar([head | tail], n) do
     [_encode(head, n) | caesar(tail, n)]
   end
@@ -23,4 +23,19 @@ defmodule MyList do
     char - n
   end
   defp _encode(char, n), do: char + n
+
+  def span(from, to) when to > from do
+    [ from | span(from + 1, to) ]
+  end
+  def span(from, to) when to < from, do: span(to, from)
+  def span(from, to) when to === from, do: [ from ]
+
+  def flatten(list), do: _flatten(list, [])
+  def _flatten([], acc), do: acc
+  def _flatten([head | tail], acc) when is_list(head) do
+    _flatten(head, acc) ++ _flatten(tail, acc)
+  end
+  def _flatten([head | tail], acc) do
+    [ head | _flatten(tail, acc) ]
+  end
 end
