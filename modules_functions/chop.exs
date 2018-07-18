820adffc1326ee9210fca78fd2ed7559b298449b
Jürgen Messner <juergen.messner@cornelsen.de>
2018-07-18 22:54:50 +0200

    minor formatting and fixed private scope for internal funcitons

--- modules_functions/chop.exs
+++ modules_functions/chop.exs
@@ -1,18 +1,18 @@
 defmodule Chop do
-  def guess(actual, low..high) do
+  def guess(actual, low..high) when actual >= low and actual <= high do
     guess = div(low + high, 2)
     process(actual, low..high, guess)
   end
-  def process(actual, _, current) when current == actual do
+  defp process(actual, _, current) when current == actual do
     IO.puts current
   end
-  def process(actual, low.._, current) when current > actual do
+  defp process(actual, low.._, current) when current > actual do
     high = current - 1
     guess = div(low + high, 2)
     IO.puts "Is it #{guess}"
     process(actual, low..high, guess)
   end
-  def process(actual, _..high, current) when current < actual do
+  defp process(actual, _..high, current) when current < actual do
     low = current + 1
     guess = div(low + high, 2)
     IO.puts "Is it #{guess}"
